244d2ec3f2305c89ae4b5c6469513c3784c8e26f
David R. Bild <drbild@tellur.io>
2015-09-02 12:27:10 -0500

    cleanup organization

--- core/src/main/scala/scintuit/data/Common.scala
+++ core/src/main/scala/scintuit/data/Common.scala
@@ -1,9 +1,9 @@
 package scintuit.data
 
 case class Address(
-  address1: Option[String],
-  address2: Option[String],
-  address3: Option[String],
+  line1: Option[String],
+  line2: Option[String],
+  line3: Option[String],
   city: Option[String],
   state: Option[String],
   postalCode: Option[String],
