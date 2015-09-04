5cf0763b03730b5e578ef6811966d93bc9fd5ca1
David R. Bild <drbild@tellur.io>
2015-09-04 18:36:47 -0500

    switch interpretter to Play json

--- core/src/main/scala/scintuit/contrib/http4s/Interpreter.scala
+++ core/src/main/scala/scintuit/contrib/http4s/Interpreter.scala
@@ -1,27 +1,23 @@
 package scintuit
 package contrib.http4s
 
-import argonaut._
-import monocle._
-import org.http4s.Uri
-import org.http4s.EntityDecoder
 import org.http4s.Http4s._
 import org.http4s.Status.ResponseClass.Successful
-import org.http4s.argonaut._
 import org.http4s.client.Client
+import org.http4s.{Method, Request, Uri}
+import play.api.libs.json._
 import scintuit.IntuitApi.IntuitOp._
 import scintuit.IntuitApi._
 import scintuit.auth.{OAuthConsumer, OAuthToken}
-import scintuit.contrib.argonaut._
-import scintuit.contrib.argonaut.apiOptics._
-import scintuit.data.{Institution, InstitutionSummary}
-import scintuit.util.argonaut.codecJson._
-import scintuit.util.argonaut.syntax.all._
+import scintuit.contrib.play.AccountFormats._
+import scintuit.contrib.play.InstitutionFormats._
+import scintuit.contrib.play.{ApiTransforms, PlayJsonInstances}
+import scintuit.data._
 
 import scalaz.concurrent.Task
 import scalaz.~>
 
-object Interpreter extends OAuthSupport {
+object Interpreter extends OAuthSupport with PlayJsonInstances with ApiTransforms {
 
   // ================================ Interpreter ================================
   def trans(client: Client, consumer: OAuthConsumer, token: OAuthToken) = {
@@ -30,16 +26,21 @@ object Interpreter extends OAuthSupport {
     /* Configuration*/
     val BASE = uri("https://financialdatafeed.platform.intuit.com/v1")
 
-    /** convert a json <-> json optic into a decoder */
-    def fromOptic[A](optional: Optional[Json, Json])(implicit decode: DecodeJson[A], encode: EncodeJson[A]):
-    EntityDecoder[A] =
-      jsonOf((optional composePrism as[A]).toDecodeJson)
-
     /** issue a get request and handle the response */
-    def get[A](path: String)(view: Optional[Json, Json])(implicit decode: DecodeJson[A], encode: EncodeJson[A]) = {
+    def get[A](path: String, transform: Reads[JsValue])(implicit reads: Reads[A]) = {
       val uri = BASE / path
       signed(uri) flatMap {
-        case Successful(res) => res.as[A](fromOptic(view))
+        //case Successful(res) => res.as[A](jsonOf(reads compose Reads(x => {println(Json.prettyPrint(x)); val y = transform.reads(x); println(Json.prettyPrint(y.get)); y})))
+        case Successful(res) => res.as[A](jsonOf(reads compose transform))
+        case res => Task.fail(IntuitAPIError(uri, res.status.code))
+      }
+    }
+
+    def delete(path: String) = {
+      val uri = BASE / path
+      val req = Request(Method.DELETE, uri)
+      signed(req) flatMap {
+        case Successful(res) => Task.now(())
         case res => Task.fail(IntuitAPIError(uri, res.status.code))
       }
     }
@@ -47,8 +48,15 @@ object Interpreter extends OAuthSupport {
     new (IntuitOp ~> Task) {
       override def apply[A](fa: IntuitOp[A]): Task[A] =
         fa match {
-          case ListInstitution => get[List[InstitutionSummary]]("institutions")(institutionsO)
-          case GetInstitution(id) => get[Institution](s"institutions/${id}")(institutionO)
+          case ListInstitutions => get[Seq[InstitutionSummary]]("institutions", listInstitutionsT)
+          case GetInstitution(id) => get[Institution](s"institutions/${id}", getInstitutionT)
+
+          case GetAccount(id) => get[Account](s"accounts/${id}", getAccountT)
+          case ListCustomerAccounts => get[Seq[Account]](s"accounts", listAccountsT)
+          case ListLoginAccounts(id) => get[Seq[Account]](s"logins/${id}/accounts", listAccountsT)
+          case DeleteAccount(id) => delete(s"accounts/${id}")
+
+          case _ => Task.fail(new RuntimeException("not implemented"))
         }
     }
   }
