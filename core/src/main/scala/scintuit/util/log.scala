3802094d456f0ad1c77cc389a8e9bfd5ec7a705d
David R. Bild <drbild@tellur.io>
2015-12-15 16:54:57 -0600

    add module for logging

---
+++ core/src/main/scala/scintuit/util/log.scala
@@ -0,0 +1,26 @@
+package scintuit.util
+
+import com.github.nscala_time.time.Imports._
+import org.log4s._
+import scintuit.customer.Customer
+import scintuit.util.capture.Capture
+import scintuit.util.http.{Response, Request}
+
+import scala.concurrent.duration.FiniteDuration
+import scalaz.Monad
+
+object log {
+
+  val httpLogger = getLogger("scintuit.http")
+
+  def logHttp[M[_]: Monad : Capture, C: Customer](
+    customer: C,
+    start: DateTime,
+    request: Request,
+    response: Response,
+    elapsed: FiniteDuration
+  ): M[Unit] = Capture[M].apply{
+      httpLogger.info(s"""${Customer[C].name(customer)} [${start.withZone(DateTimeZone.UTC).toString}] "${request.method} ${request.uri}" ${response.status} ${elapsed.toMillis}""")
+    }
+
+}
